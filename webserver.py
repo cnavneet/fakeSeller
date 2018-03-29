169374ee69e4269b871e990beb34e2f2c6fa5262
navneet choudhary <cnavneet58@gmail.com>
2018-03-29 09:24:52 +0530

    changing back end

--- webserver.py
+++ webserver.py
@@ -79,6 +79,8 @@ def valid_email(email):
 
 @app.route('/',methods=['POST','GET'])
 def signin():
+	if request.cookies.get('user_id'):
+		return redirect(url_for('profile'))
 	if request.method=='POST':
 		username=request.form['username']
 		password=request.form['password']
@@ -152,6 +154,37 @@ def profile():
 	else:
 		return redirect(url_for('signin'))
 
+@app.route('/profile/<int:user_id>/<int:seller_id>/sdelete',methods=['GET','POST'])
+def sdelete(user_id,seller_id):
+	if not request.cookies.get('user_id'):
+		return redirect(url_for('signin'))
+	brand=session.query(Brand).filter_by(id=user_id).one()
+	seller=session.query(Seller).filter_by(parent_id=user_id,id=seller_id).one()
+	if request.method=='POST':
+		session.delete(seller)
+		session.commit()
+		flash("Successfully deleted "+seller.name+" registered for "+seller.product+" !!")
+
+		return redirect(url_for('profile'))
+
+	return render_template('sdelete.html',brand=brand,seller=seller)
+
+@app.route('/profile/<int:user_id>/<int:product_id>/delete',methods=['GET','POST'])
+def pdelete(user_id,product_id):
+	if not request.cookies.get('user_id'):
+		return redirect(url_for('signin'))
+	brands=session.query(Brand).filter_by(id=user_id).one()
+	brandItem=session.query(SubBrand).filter_by(id=product_id,parent_id=user_id).one()
+	if request.method=='POST':
+		Pdel=session.query(SubBrand).filter_by(id=product_id,parent_id=user_id).one()
+		session.delete(Pdel)
+		session.commit()
+		flash("Sub-brand/Product deleted!!")
+
+		return redirect(url_for('profile'))
+
+	return render_template('pdelete.html',brand=brands,sbrand=brandItem)
+
 @app.route('/profile/<int:user_id>/<int:product_id>/edit',methods=['POST','GET'])
 def pedit(user_id,product_id):
 	if not request.cookies.get('user_id'):
@@ -169,6 +202,35 @@ def pedit(user_id,product_id):
 
 	return render_template('bedit.html',brand=brands,sbrand=brandItem)
 
+@app.route('/profile/<int:user_id>/<int:seller_id>/sedit',methods=['GET','POST'])
+def sedit(user_id,seller_id):
+	if not request.cookies.get('user_id'):
+		return redirect(url_for('signin'))
+	brand=session.query(Brand).filter_by(id=user_id).one()
+	sub=session.query(SubBrand).filter_by(parent_id=user_id).all()
+	seller=session.query(Seller).filter_by(parent_id=user_id,id=seller_id).one()
+
+	if request.method=='POST':
+		name=request.form['name']
+		product=request.form['product']
+		price=request.form['price']
+
+		sb=session.query(SubBrand).filter_by(parent_id=user_id,name=product).all()
+		if not sb:
+			msg="The sub-brand/product is'nt in the current list of "+brand.name
+			return render_template('selledit.html',msg=msg,brand=brand,sub=sub,seller=seller)
+
+		seller.name=name
+		seller.product=product
+		seller.price=price
+		session.add(seller)
+		session.commit()
+
+		flash("Successfully editted "+seller.name)
+		return redirect(url_for('profile'))
+
+	return render_template('selledit.html',brand=brand,sub=sub,seller=seller)
+
 @app.route('/digitaladmin',methods=['POST','GET'])
 def admin():
 	if request.method=='POST':
@@ -180,6 +242,15 @@ def admin():
 		msg="Inavlid admin credentials!!"
 		return render_template('admin.html',msg=msg)
 	return render_template('admin.html')
+
+@app.route('/adminpage/<int:brand_id>/addelete')
+def addel(brand_id):
+	brand=session.query(Brand).filter_by(id=brand_id).one()
+	session.delete(brand)
+	session.commit()
+
+	flash(brand.name+" successfully deleted!!")
+	return redirect(url_for('adminpage'))
 
 @app.route('/branddetail/<int:brand_id>/')
 def branddetail(brand_id):
